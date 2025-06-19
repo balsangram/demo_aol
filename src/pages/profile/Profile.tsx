fc0b9a9ac3d26bd68357da33de1980704c1d46a4
balsangram <balsangram1@gmail.com>
2025-06-19 17:03:50 +0530

    first commit

---
+++ src/pages/profile/Profile.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import profile from '../../assets/images/profile.png';
+
+function Profile() {
+    return (
+        <>
+            <div>
+                <img src={profile} alt="" />
+            </div>
+            <div></div>
+        </>
+    );
+}
+
+export default Profile;
